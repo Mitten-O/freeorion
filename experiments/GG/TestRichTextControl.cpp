04fd20e9ff3e060495ce110eb8fe864d7e6feed2
Mitten-O <Mitten.O@gmail.com>
2015-09-08 21:22:04 +0300

    Update test to exercise ImageBlock properly.
    Use src="path".
    Test the default path capability.
    Add invalid image to test the fallback.

--- experiments/GG/TestRichTextControl.cpp
+++ experiments/GG/TestRichTextControl.cpp
@@ -5,6 +5,7 @@
 #include <GG/StyleFactory.h>
 
 #include <GG/RichText/RichText.h>
+#include <GG/RichText/ImageBlock.h>
 
 #include "TestApp.h"
 #include "TestDialog.h"
@@ -21,14 +22,19 @@ int main(int argc, char* argv[])
     const boost::shared_ptr<GG::Font> font =
     GG::GUI::GetGUI()->GetStyleFactory()->DefaultFont(12, &charsets[0], &charsets[0] + charsets.size());
 
+    GG::ImageBlock::SetDefaultImagePath("../../default/");
+
+    // Test the rich text box.
+    // Not also how the tags cannot be recursive yet, not even with
+    // native tags, only root level images work.
     GG::Wnd* box = new GG::RichText(GG::X(10), GG::Y(10), GG::X(400), GG::Y(300),
                                     "The essence of a true text layout lies in <i>its capability</i> to fit the text into regions it determines "
                                     "from the other things it believes to be relevat to the layout of the text in the regions to which it is laying out the text. "
-                                    "This figure should clarify the matter: <img \"../../default/data/art/icons/combat.png\"></img> As can be seens from that glorious exepelar, "
-                                    "much truth is reachable by the human mind only given the information it comprises is in a form that can be made into "
+                                    "This figure should clarify the matter: <img src=\"data/art/icons/combat.png\"></img> As can be seens from that glorious exepelar, "
+                                    "much truth is reachable by the human mind<img src=\"data/does.not.exist.png\"></img> only given the information it comprises is in a form that can be made into "
                                     " truth by the human thought organ.\n"
                                     "Nevertheless, true <confusing>of people can only <i>arise "
-                                    "<img \"../../default/data/art/icons/combat.png\"></img>"
+                                    "<img src=\"data/art/icons/combat.png\"></img>"
                                     " from constant<u>struggle <rgba 100 200 250 255>and</rgba> strife</u> as bill </i> said.</confusing>"
                                     , font, GG::CLR_GREEN);
 
